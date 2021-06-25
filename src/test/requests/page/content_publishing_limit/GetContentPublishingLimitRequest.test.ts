c9e479be09eaaea58aa0373ef4246828c61f75ab
TiagoGrosso <tiagogrosso99@gmail.com>
2021-06-25 09:31:04 +0100

    Add GetContentPublishingLimitRequest

--- src/test/requests/page/content_publishing_limit/GetContentPublishingLimitRequest.test.ts
+++ src/test/requests/page/content_publishing_limit/GetContentPublishingLimitRequest.test.ts
@@ -2,7 +2,10 @@ import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import { Constants } from '../../../../main/Constants';
 import { ContentPublishingLimitFields } from '../../../../main/Enums';
-import { GetContentPublishingLimitRequest } from '../../../../main/requests/page/content_publishing_limit/GetContentPublishingLimitRequest';
+import {
+    DateOlderThanOneDay,
+    GetContentPublishingLimitRequest,
+} from '../../../../main/requests/page/content_publishing_limit/GetContentPublishingLimitRequest';
 import { GetContentPublishingLimitResponse } from '../../../../main/requests/page/content_publishing_limit/GetContentPublishingLimitResponse';
 import { TestConstants } from '../../../TestConstants';
 
@@ -22,6 +25,23 @@ describe('GetContentPublishingLimitRequest', () => {
         expect(request.config().method).toEqual('GET');
         expect(request.config().url).toEqual(`/${TestConstants.PAGE_ID}/content_publishing_limit`);
         expect(requestAllFields.config().params.fields).toEqual(Object.values(ContentPublishingLimitFields).join(','));
+    });
+
+    it('Adds a "since" param', () => {
+        const date: Date = new Date();
+        request.since(date);
+        expect(request.config().params.since).toEqual(Math.floor(date.getTime() / 1000));
+    });
+
+    it('Throws on an invalid "since" date', () => {
+        const date: Date = new Date('20 Dec 1980');
+        expect(() => request.since(date)).toThrowError(new DateOlderThanOneDay(date));
+    });
+
+    it('Throws on trying to add a range', () => {
+        expect(() => request.addRange(new Date(), new Date())).toThrowError(
+            new Error('For GetContentPublishingLimitRequest, use "since(date)" instead.')
+        );
     });
 
     const mock = new MockAdapter(axios);
