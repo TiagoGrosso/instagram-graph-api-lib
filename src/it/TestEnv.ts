a29e9a13a39adda04a77146fc82086cb653034b5
Tiago Grosso <tiagogrosso99@gmail.com>
2022-11-06 15:11:41 +0000

    Refactor publish pipeline

--- src/it/TestEnv.ts
+++ src/it/TestEnv.ts
@@ -3,16 +3,25 @@ import { Client } from '../main/Client';
 
 dotenv.config();
 
+/**
+ * Gets the page id.
+ */
 export function getPageId(): string {
     return process.env.PAGE_ID ?? '';
 }
 
+/**
+ * Gets the page access token.
+ */
 export function getPageAccessToken(): string {
     return process.env.PAGE_ACCESS_TOKEN ?? '';
 }
 
 const client = new Client(getPageAccessToken(), getPageId());
 
+/**
+ * Gets the client to create requests.
+ */
 export function getClient(): Client {
     return client;
 }
